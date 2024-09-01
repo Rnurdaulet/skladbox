9a20b01f2bd14a12674667c329af81447d054304
Rnurdaulet <rakhymberdin@gmail.com>
2024-09-02 00:14:48 +0500

    Base design fix

--- accounts/views.py
+++ accounts/views.py
@@ -6,8 +6,10 @@ from django.contrib.auth.forms import AuthenticationForm
 from .forms import UserRegisterForm
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'accounts/home.html')
+
 
 def register(request):
     if request.method == 'POST':
@@ -23,7 +25,9 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
 
+
 def login_view(request):
+    error_message = None
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
@@ -33,9 +37,15 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 return redirect('home')
+            else:
+                error_message = 'Неправильный логин или пароль'
+        else:
+            error_message = 'Неправильный логин или пароль'
     else:
         form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form': form})
+
+    return render(request, 'accounts/login.html', {'form': form, 'error_message': error_message})
+
 
 def logout_view(request):
     logout(request)
