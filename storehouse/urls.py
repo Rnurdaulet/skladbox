9d7ca77bb2c320855b142aaffdee555ab7c89d1a
Rnurdaulet <rakhymberdin@gmail.com>
2024-09-03 12:21:23 +0500

    kaspi

--- storehouse/urls.py
+++ storehouse/urls.py
@@ -2,12 +2,15 @@
 
 from django.urls import path
 from . import views
-from .views import generate_pdf
+from .views import generate_pdf, get_offer_view, get_archive_view, get_points
 
 urlpatterns = [
     path('create/', views.create_record, name='create_record'),
     path('records/', views.record_list, name='record_list'),
     path('records/<int:pk>/', views.record_detail, name='record_detail'),
     path('record/<int:record_id>/pdf/', generate_pdf, name='generate_pdf'),
+    path('offer-view/', get_offer_view, name='offer_view'),
+    path('archive-view/', get_archive_view, name='archive_view'),
+    path('points/', get_points, name='points'),
 ]
 
--- storehouse/views.py
+++ storehouse/views.py
@@ -1,5 +1,4 @@
 # storehouse/views.py
-
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import StorehouseRecordForm
 from .models import StorehouseRecord
@@ -54,3 +53,116 @@ def record_list(request):
 def record_detail(request, pk):
     record = get_object_or_404(StorehouseRecord, pk=pk)
     return render(request, 'storehouse/record_detail.html', {'record': record})
+
+import requests
+from django.http import JsonResponse
+import time
+
+USERNAME = 'sks_techno@mail.ru'
+PASSWORD = 'Password1'
+HEADERS = {
+    'Content-Type': 'application/json; charset=utf-8',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, как Gecko) '
+                  'Chrome/91.0.4472.124 Safari/537.36'
+}
+M_PARAM = '18604100'
+
+
+def get_kaspi_cookies():
+    login_url = 'https://kaspi.kz/mc/api/login'
+    params = {
+        'username': USERNAME,
+        'password': PASSWORD
+    }
+
+    try:
+        response = requests.post(login_url, params=params,
+                                 headers={k: v.encode('utf-8') if isinstance(v, str) else v for k, v in
+                                          HEADERS.items()})
+        if response.status_code == 200:
+            return response.cookies
+        else:
+            print(f"Failed to login. Status code: {response.status_code}, Response: {response.text}")
+            return None
+    except requests.RequestException as e:
+        print(f"Request exception: {str(e)}")
+        return None
+
+
+def make_kaspi_request(url, params, headers, cookies, max_retries=5):
+    retry_delay = 2  # секунды
+
+    for attempt in range(max_retries):
+        try:
+            response = requests.get(url, params=params,
+                                    headers={k: v.encode('utf-8') if isinstance(v, str) else v for k, v in
+                                             headers.items()}, cookies=cookies)
+
+            if response.status_code == 200:
+                return response.json(), 200
+            elif response.status_code == 429:
+                time.sleep(retry_delay)
+                retry_delay *= 2  # Exponential backoff
+            else:
+                return {'error': f'Failed to get data from Kaspi API. Status code: {response.status_code}'}, response.status_code
+        except requests.RequestException as e:
+            return {'error': f'An exception occurred: {str(e)}'}, 500
+
+    return {'error': 'Failed to get data from Kaspi API after multiple attempts'}, 429
+
+
+def fetch_kaspi_data(view_url, params):
+    cookies = get_kaspi_cookies()
+
+    if cookies is None:
+        return JsonResponse({'error': 'Failed to get cookies from Kaspi API'}, status=500)
+
+    result, status_code = make_kaspi_request(view_url, params, HEADERS, cookies)
+    return JsonResponse(result, status=status_code if status_code != 200 else 200, safe=False)
+
+
+def get_offer_view(request):
+    offer_view_url = 'https://mc.shop.kaspi.kz/bff/offer-view/list'
+    params = {
+        'm': M_PARAM,
+        'p': 0,
+        'l': 10,
+        'a': 'true',
+        't': '',
+        'c': ''
+    }
+    return fetch_kaspi_data(offer_view_url, params)
+
+
+def get_archive_view(request):
+    archive_view_url = 'https://mc.shop.kaspi.kz/mc/api/orderTabs/archive'
+    params = {
+        'start': 0,
+        'count': 10,
+        'fromDate': 1722970800000,
+        'toDate': 1723027971803,
+        'statuses': ['CANCELLED', 'COMPLETED', 'RETURNED', 'CREDIT_TERMINATION_PROCESS'],
+        '_m': M_PARAM
+    }
+    return fetch_kaspi_data(archive_view_url, params)
+
+
+def get_points(request):
+    points_url = 'https://mc.shop.kaspi.kz/bff/points'
+    params = {
+        'merchantUid': M_PARAM
+    }
+    return fetch_kaspi_data(points_url, params)
+
+# (selected_tab = PICKUP,SIGN_REQUIRED,NEW,DELIVERY,
+#  KASPI_DELIVERY_WAIT_FOR_POINT_DELIVERY,KASPI_DELIVERY_CARGO_ASSEMBLY,
+#  KASPI_DELIVERY_WAIT_FOR_COURIER,KASPI_DELIVERY_TRANSMITTED,KASPI_DELIVERY_RETURN_REQUEST)
+def get_active_orders(request, selected_tab):
+    active_orders_url = 'https://mc.shop.kaspi.kz/mc/api/orderTabs/active'
+    params = {
+        'count': 10,
+        'selectedTabs': selected_tab,
+        'startIndex': 0,
+        '_m': M_PARAM
+    }
+    return fetch_kaspi_data(active_orders_url, params)
